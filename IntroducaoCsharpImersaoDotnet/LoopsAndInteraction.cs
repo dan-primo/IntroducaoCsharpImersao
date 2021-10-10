8246e3bcbeb7b0f9529f44880ccebe44ab244536
Danielly Primo <60229135+dan-primo@users.noreply.github.com>
2021-10-10 11:53:38 -0300

    loops-interaction

---
+++ IntroducaoCsharpImersaoDotnet/LoopsAndInteraction.cs
@@ -0,0 +1,23 @@
+using System;
+
+namespace IntroducaoCsharpImersaoDotnet
+{
+    public class LoopsAndInteraction
+    {
+        public static void Main(string[] args)
+        {
+            var name = "John";
+            var age = 12;
+            var ofLegalAge = age >= 18;
+
+            var numbers = new int[] {1, 2, 3, 4 };
+
+            foreach (var temp in numbers)
+            {
+                Console.WriteLine(temp);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
