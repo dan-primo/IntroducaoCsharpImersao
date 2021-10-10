180b4f1f5c12ed079c170de0b898003d1b739ce6
Danielly Primo <60229135+dan-primo@users.noreply.github.com>
2021-10-10 11:22:07 -0300

    basic-sintax

---
+++ IntroducaoCsharpImersaoDotnet/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace introducao_csharp_imersao_dotnet
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            var name = "Yan";
+            var age = 0;
+
+            var ofLegalAge = age > 18;
+
+            if (ofLegalAge)
+            {
+                Console.WriteLine(name + "The person is of legal age.");
+            }
+            else if (age == 0) {
+                Console.WriteLine("Invalid Age");
+            }
+            else {
+                Console.WriteLine(name + "The person is not of legal age.");
+            }
+
+            Console.WriteLine("The" + name + "is" + age + "years old.");
+
+            Console.ReadLine();
+        }
+    }
+}
